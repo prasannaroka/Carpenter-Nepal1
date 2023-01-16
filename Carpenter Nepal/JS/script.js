2dda22de2f155b6c91eb2ad71d13ca4301360f5f
prasannaroka <120256128+prasannaroka@users.noreply.github.com>
2023-01-16 15:20:39 +0545

    Add files via upload

---
+++ Carpenter Nepal/JS/script.js
@@ -0,0 +1,17 @@
+// console.log("script running...")
+// document.querySelector('.fa-solid fa-bars').style.display = 'none';
+// document.querySelector('.fa-solid fa-bars').addEventListener("click", ()=>{
+//     document.querySelector('.menu-bar').classList.toggle('active');
+//     if(document.querySelector('.menu-bar').classList.contains('active')){
+//         document.querySelector('.fa-solid fa-bars').style.display = 'inline';
+//         document.querySelector('.menu-bar').style.display = 'none';
+//     }
+//     else{
+//         document.querySelector('.fa-solid fa-bars').style.display = 'none'
+//         setTimeout(() => {
+//             document.querySelector('.menu-bar').style.display = 'inline'
+//       }, 400);
+//     }
+
+
+// })
